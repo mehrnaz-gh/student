7a1703581aa5d4c5e3066d66a26cd434d3796f90
mehrnaz-gh <62984993+mehrnaz-gh@users.noreply.github.com>
2020-04-16 19:33:10 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ormt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext())
+            {
+                context.Database.CreateIfNotExsits();
+                if (context.Student.Any())
+                {
+                   
+                    object name = string.Name;
+                }
+                var Students = context.Student;
+                foreach(var item in Student)
+                {
+                    Console.WriteLine("ID = " + item.Id);
+                    Console.WriteLine("Name " + item.Name);
+                    Console.WriteLine("Email" + item.Email);
+                }
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
